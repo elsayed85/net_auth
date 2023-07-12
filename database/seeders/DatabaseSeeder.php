4dd4528d0e70a266181379753492b5ccba202cab
elsayed kamal <elsayedkamal581999@gmail.com>
2023-07-12 17:31:34 +0300

    “wip”

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $sql = storage_path("app/data.sql");
+        DB::unprepared(file_get_contents($sql));
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            "name" => "Sayed",
+            "phone" => "01092291556",
+            "password" => Hash::make("password"),
+        ]);
     }
 }
